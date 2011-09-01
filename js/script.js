0b2293f8b30910c1bb1c1dc64b03098737e90279
Sebastian Ruml <sebastianruml@gmail.com>
2011-09-01 20:05:24 +0200

    Initial import

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/* Author: 
+
+*/
+
+// execute your scripts when the DOM is ready. this is mostly a good habit
+$(document).ready(function() {
+  $(".scrollablecontainer").scrollable();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
